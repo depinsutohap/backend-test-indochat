63b4a9c1e433846eaaf6701e5082164e9965a867
siens <sienshum@gmail.com>
2022-06-09 14:21:49 +0700

    Markdown demo dependency

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"fmt"
 	
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
@@ -22,11 +23,13 @@ func main() {
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
-		 yourHtmlString := "<html><body>I am cached HTML!<br>Hi<br></body></html>"
+		yourHtmlString := ""
+		number := 99
+		for i := 1; i < 11; i++ {
+			yourHtmlString += fmt.Sprintf("%d * %d = %d\n", i, number, number * i)
+		  }
 
-		//Write your 200 header status (or other status codes, but only WriteHeader once)
 		c.Writer.WriteHeader(http.StatusOK)
-		//Convert your cached html string to byte array
 		c.Writer.Write([]byte(yourHtmlString))
 	})
 
